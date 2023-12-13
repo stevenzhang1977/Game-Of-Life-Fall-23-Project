a83582960ba73e56fa204ffdf37b820b1a7ed065
stevenzhang1977 <153657475+stevenzhang1977@users.noreply.github.com>
2023-12-12 22:44:38 -0500

    Delete GOLModelInterface.java

--- GOLModelInterface.java
+++
@@ -1,9 +0,0 @@
-public interface GOLModelInterface {
-        int getRows();
-        int getColumns();
-        void nextGeneration();
-        int liveNeighbors(int row, int column);
-        void loadPatternFile(String fileName);
-        boolean[][] getGrid();
-
-}
